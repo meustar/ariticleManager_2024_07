52622c876360c626a5a9d529343efa8bcffda829
meustar <meustar829@gmail.com>
2024-07-04 16:41:07 +0900

    service 도입, Container 초기화, 역할분담

--- src/main/java/org/koreait/articleManager/Main.java
+++ src/main/java/org/koreait/articleManager/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Container.init();
 
         new App().run();
 
--- src/main/java/org/koreait/controller/ArticleController.java
+++ src/main/java/org/koreait/controller/ArticleController.java
@@ -3,6 +3,8 @@ package org.koreait.controller;
 import org.koreait.articleManager.Container;
 import org.koreait.dto.Article;
 import org.koreait.dto.Member;
+import org.koreait.service.ArticleService;
+import org.koreait.service.MemberService;
 import org.koreait.util.Util;
 
 import java.util.ArrayList;
@@ -11,18 +13,25 @@ import java.util.Scanner;
 
 public class ArticleController extends Controller {
     private Scanner sc;
-    private List<Article> articles;
+//    private List<Article> articles;   // Controller -> Service -> Dao 에서 처리할수 있게 되야한다. 직접처리하면 안됨.
     private String cmd;
+    private List<Member> members;
 
-    List<Member> members = Container.memberDao.members;
+    private ArticleService articleService;
+    private MemberService memberService;
 
     private int lastArticleId = 3;
 
-    public ArticleController(Scanner sc) {
 
+
+
+    public ArticleController(Scanner sc) {
+//        articles = Container.articleDao.articles;       // 자바기초 42강 후반부.. .두번으로 넘겨넘겨 가는 방법
+        this.articleService = Container.articleService;
+        this.memberService = Container.memberService;
         this.sc = sc;
 //        articles = new ArrayList<>();
-        articles = Container.articleDao.articles;       // 자바기초 42강 후반부.. .두번으로 넘겨넘겨 가는 방법
+        members = memberService.getMembers();
     }
 
     public void doAction(String cmd, String actionMethodName) {
@@ -65,7 +74,7 @@ public class ArticleController extends Controller {
         String body = sc.nextLine();
 
         Article article = new Article(id, regDate, updateDate, loginedMember.getId(), title, body);
-        articles.add(article);
+        articleService.add(article);
 
         System.out.println(id + "번 글이 생성되었습니다");
         lastArticleId++;
@@ -74,32 +83,16 @@ public class ArticleController extends Controller {
 
     private void showList() {
         System.out.println("==게시글 보기==");
-        if (articles.size() == 0) {
+        if (articleService.getSize() == 0) {
             System.out.println("아무것도 없어");
             return;
         }
 
         String searchKeyword = cmd.substring("article list".length()).trim();
 
-        List<Article> forPrintArticles = articles;
-
-        if (searchKeyword.length() > 0) {
-            System.out.println("검색어 : " + searchKeyword);
-            forPrintArticles = new ArrayList<>();
+        List<Article> forPrintArticles = articleService.getForPrintAricles(searchKeyword);
 
 
-            for (Article article : articles) {
-                if (article.getTitle().contains(searchKeyword)) {
-                    forPrintArticles.add(article);
-
-                }
-            }
-            if (forPrintArticles.size() == 0) {
-                System.out.println("  번호   /    날짜    /  작성자  /   제목   /   내용   ");
-                System.out.println("검색 결과 없음");
-                return;
-            }
-        }
 
         String writerName = null;
 
@@ -141,7 +134,7 @@ public class ArticleController extends Controller {
 
         int id = Integer.parseInt(cmd.split(" ")[2]);
 
-        Article foundArticle = getArticleById(id);
+        Article foundArticle = articleService.getArticleById(id);
 
         if (foundArticle == null) {
             System.out.println("해당 게시글은 없습니다");
@@ -162,7 +155,7 @@ public class ArticleController extends Controller {
 
         int id = Integer.parseInt(cmd.split(" ")[2]);
 
-        Article foundArticle = getArticleById(id);
+        Article foundArticle = articleService.getArticleById(id);
 
         if (foundArticle == null) {
             System.out.println("해당 게시글은 없습니다");
@@ -172,7 +165,7 @@ public class ArticleController extends Controller {
             System.out.println("권한 없음");
             return;
         }
-        articles.remove(foundArticle);
+        articleService.remove(foundArticle);
         System.out.println(foundArticle.getId() + "번 게시글이 삭제되었습니다");
     }
 
@@ -181,7 +174,7 @@ public class ArticleController extends Controller {
 
         int id = Integer.parseInt(cmd.split(" ")[2]);
 
-        Article foundArticle = getArticleById(id);
+        Article foundArticle = articleService.getArticleById(id);
 
         if (foundArticle == null) {
             System.out.println("해당 게시글은 없습니다");
@@ -207,22 +200,14 @@ public class ArticleController extends Controller {
         System.out.println(foundArticle.getId() + "번 게시글이  수정되었습니다");
     }
 
-    private Article getArticleById(int id) {
-        // 위 for문과 같음. 더 간략화 될뿐.
-        for (Article article : articles) {
-            if (article.getId() == id) {
-                return article;
-            }
-        }
-        return null;
-    }
+
 
     public void makeTestData() {
         System.out.println("게시글 테스트 데이터 생성");
 
-        articles.add(new Article(1, "2023-12-12-12 12:12:12", "2023-12-12-12 12:12:12", 1, "제목1", "내용1"));
-        articles.add(new Article(2, Util.getNow(), Util.getNow(), 1,"제목2", "내용2"));
-        articles.add(new Article(3, Util.getNow(), Util.getNow(), 2,"제목3", "내용3"));
+        articleService.add(new Article(1, "2023-12-12-12 12:12:12", "2023-12-12-12 12:12:12", 1, "제목1", "내용1"));
+        articleService.add(new Article(2, Util.getNow(), Util.getNow(), 1,"제목2", "내용2"));
+        articleService.add(new Article(3, Util.getNow(), Util.getNow(), 2,"제목3", "내용3"));
     }
 
 }
